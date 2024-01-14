cbc3e574318df7b8573130df4f3b7312ba051fff
Thib-numberX <peronnothibault@hotmail.fr>
2024-01-14 16:11:00 +0100

    added icon delete

--- lib/components/task_card.dart
+++ lib/components/task_card.dart
@@ -13,16 +13,27 @@ class TaskCard extends StatelessWidget {
     // we can not use read directly in the action onchanged or onPressed. Maybe others. So We create a variable before.
     final taskUpdate = context.read<TasksBloc>();
     print(taskUpdate);
-    return Card(
-      child: CheckboxListTile(
-        title: Text(task.description),
-        value: task.isDone == 1 ? true : false,
-        onChanged: (value) {
-          print(value);
-          taskUpdate.add(UpdateTaskEvent(task.id, value!, task.description));
-          // value == true ? task.isDone = 1 : task.isDone = 0;
-        },
-      ),
+    return Row(
+      children: [
+        Expanded(
+          child: CheckboxListTile(
+            title: Text(task.description),
+            value: task.isDone == 1 ? true : false,
+            onChanged: (value) {
+              print(value);
+              taskUpdate
+                  .add(UpdateTaskEvent(task.id, value!, task.description));
+              // value == true ? task.isDone = 1 : task.isDone = 0;
+            },
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            print('delete task $task');
+          },
+          icon: const Icon(Icons.delete_forever_rounded),
+        ),
+      ],
     );
   }
 }
